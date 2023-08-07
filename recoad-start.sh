c1c54f9150505929cf1c4c76b38b4eb140de8e8e
noli-noli <gp20a051@oecu.jp>
2023-08-07 18:58:11 +0900

    絶対パスから相対パスに

--- recoad-start.sh
+++ recoad-start.sh
@@ -1,2 +1,2 @@
 screen -d -m -S tmp-recoad
-screen -S tmp-recoad -X stuff "python3 /home/pi/tmp-recoad/temperature_recoad_v1.1.py\n"
+screen -S tmp-recoad -X stuff "python3 temperature_recoad_v1.1.py\n"
